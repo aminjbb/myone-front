8dc9042b63bcc0d836e0e2d23b3a1e9892d60f44
ast2019 <ezel.kaya.tr@gmail.com>
2025-03-14 16:50:15 +0330

    Create gsap.js

---
+++ plugins/gsap.js
@@ -0,0 +1,10 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  if (process.client) {
+    gsap.registerPlugin(ScrollTrigger);
+    nuxtApp.provide('gsap', gsap);
+    nuxtApp.provide('ScrollTrigger', ScrollTrigger);
+  }
+});
